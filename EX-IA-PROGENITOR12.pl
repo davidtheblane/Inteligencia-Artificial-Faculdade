e82400c19ffcf8f152a1e178e4408e00113fcc27
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2019-04-11 10:29:54 -0300

    Adicionando novos predicados.

--- EX-IA-PROGENITOR12.pl
+++ EX-IA-PROGENITOR12.pl
@@ -19,21 +19,22 @@ irmao(FILHO1, FILHO2, GENITOR) :-
     genitor(GENITOR, FILHO2),
     homem(FILHO1),
     homem(FILHO2),
-    FILHO1 \= FILHO2.
+    FILHO2 \= FILHO1.
 
 irma(FILHA1, FILHA2, GENITOR) :-
     genitor(GENITOR, FILHA1),
     genitor(GENITOR, FILHA2),
     mulher(FILHA1),
     mulher(FILHA2),
-    FILHA1 \= FILHA2.
+    FILHA2 \= FILHA1.
 
 filho_unico(FILHO1, GENITOR) :-
     not(irmao(FILHO1, _, GENITOR)),
     genitor(FILHO1, GENITOR),
     homem(FILHO1).
 
-all :- read(X),
-       write(X), write('  '), write(_FILHO2), write(' '),
-       write(_GENITOR),
+all :- irmao(FILHO1, FILHO2, _GENITOR),
+       write(FILHO1), write('  '), nl,
+       write(FILHO2), write('  '), nl,
        fail.
+       all.
